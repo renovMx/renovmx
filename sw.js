8bad142eca89693327a0a46306dbf39491ded6e6
renovMx <grupo.cosur@gmail.com>
2026-05-25 13:15:06 -0600

    sw.js

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'renovmx-v1';
+const ASSETS = [
+  'https://github.io',
+  'index.html',
+  'logotipo.png'
+];
+
+// Instalar el Service Worker y guardar en caché los archivos básicos
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Responder desde la caché si no hay internet
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
